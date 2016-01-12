9ae6d7262f1b7ff3d3c180d6e32de7f8918c2f48
Olivier Poitrey <rs@dailymotion.com>
2016-01-12 10:06:52 -0800

    Bump revision again (got pypi issue)

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name='scanman',
     packages=['scanman'],
-    version='0.2',
+    version='0.3',
     description='A ScanSnap manager for Raspbery Pi',
     author='Olivier Poitrey',
     author_email='rs@rhapsodyk.net',
     url='https://github.com/rs/scanman',
-    download_url='https://github.com/rs/scanman/tarball/0.1',
+    download_url='https://github.com/rs/scanman/tarball/0.3',
     install_requires=['kivy', 'python-sane', 'img2pdf', 'pyyaml'],
     package_data={'scanman': ['*.kv']},
     entry_points={'console_scripts': [
